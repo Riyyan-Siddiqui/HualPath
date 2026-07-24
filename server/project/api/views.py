a1e1cffb41cf043e0336f4aad1c2807433b10d19
Riyyan Siddiqui <riyyan.s.24@gmail.com>
2026-07-24 23:44:10 +0500

    solving deployment issues

--- server/project/api/views.py
+++ server/project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 
 from services.trip_planner_service import TripPlannerService
 
-from server.project.project.api.serializers.request import TripPlannerSerializer
+from server.project.api.serializers.request import TripPlannerSerializer
 
 class PlanTripAPIView(APIView):
 
